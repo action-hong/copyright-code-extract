1dfdff0f24166b8bab16ba5c67886f197de43d03
kkopitehong <kkopite.hong@smartcorelink.com>
2023-10-16 15:57:11 +0800

    feat: add version and help option

--- src/cli.ts
+++ src/cli.ts
@@ -1,20 +1,48 @@
+import process from 'node:process'
 import { loadConfig } from 'unconfig'
+import minimist from 'minimist'
+import { version } from '../package.json'
 import type { Option } from './type'
 import { cce } from '.'
 
 async function main() {
-  const {
-    config,
-  } = await loadConfig<Option>({
-    sources: [
-      {
-        files: 'cce.config',
-        extensions: ['ts', 'mts', 'cts', 'js', 'mjs', 'cjs', 'json'],
-      },
-    ],
+  const argv = minimist(process.argv.slice(2), {
+    boolean: ['version', 'help'],
+    alias: {
+      version: 'v',
+      help: 'h',
+    },
   })
 
-  cce(config)
+  if (argv.version) {
+    // eslint-disable-next-line no-console
+    console.log(version)
+  }
+  else if (argv.help) {
+    // eslint-disable-next-line no-console
+    console.log(`
+Usage: cce [options]
+
+Options:
+  -v, --version  output the version number
+  -h, --help     display help for command
+  None           generate docx file
+`)
+  }
+  else {
+    const {
+      config,
+    } = await loadConfig<Option>({
+      sources: [
+        {
+          files: 'cce.config',
+          extensions: ['ts', 'mts', 'cts', 'js', 'mjs', 'cjs', 'json'],
+        },
+      ],
+    })
+
+    cce(config)
+  }
 }
 
 main()
